d54b0acd12740f33f33d1a96f7b877119f39bf36
Crowdbotics <team@crowdbotics.com>
2021-08-27 09:00:21 +0000

    Initial commit.

---
+++ backend/harmonymerchandiser_30146/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for harmonymerchandiser_30146 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harmonymerchandiser_30146.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "harmonymerchandiser_30146Identifier",
+  url: "https://harmonymerchandiser_30146Identifier.botics.co",
+  api: "https://harmonymerchandiser_30146Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
